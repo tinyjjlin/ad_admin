7efe2eae35e0d283d20cf7350dd72047a2175ff1
tinylin <tinyjjlin@163.com>
2018-12-05 03:42:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ad admin
+
+-----
+##  content
+采用springboot+ldap 实现对Windows AD域管理。
---
+++ src/main/java/com/tiny/ad_admin/AdAdminApplication.java
@@ -0,0 +1,12 @@
+package com.tiny.ad_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdAdminApplication.class, args);
+    }
+}
